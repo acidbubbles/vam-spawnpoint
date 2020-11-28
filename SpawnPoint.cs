9b3fc7c7955eac94019bfce7e1751b522e215210
Acid Bubbles <acidbubbles.anon@gmail.com>
2020-11-28 14:07:26 -0500

    This should work

--- SpawnPoint.cs
+++ SpawnPoint.cs
@@ -1,52 +1,34 @@
-using System;
+using UnityEngine;
 
 public class SpawnPoint : MVRScript
 {
+    private JSONStorableBool _spawnOnEnable;
+
     public override void Init()
     {
-        try
-        {
-            SuperController.LogMessage($"{nameof(SpawnPoint)} initialized");
-        }
-        catch (Exception e)
-        {
-            SuperController.LogError($"{nameof(SpawnPoint)}.{nameof(Init)}: {e}");
-        }
+        var spawnNow = new JSONStorableAction("SpawnNow", SpawnNow);
+        RegisterAction(spawnNow);
+
+        _spawnOnEnable = new JSONStorableBool("SpawnOnEnable", false);
+        RegisterBool(_spawnOnEnable);
     }
 
     public void OnEnable()
     {
-        try
-        {
-            SuperController.LogMessage($"{nameof(SpawnPoint)} enabled");
-        }
-        catch (Exception e)
-        {
-            SuperController.LogError($"{nameof(SpawnPoint)}.{nameof(OnEnable)}: {e}");
-        }
+        if(_spawnOnEnable.val)
+            SpawnNow();
     }
 
-    public void OnDisable()
+    private void SpawnNow()
     {
-        try
-        {
-            SuperController.LogMessage($"{nameof(SpawnPoint)} disabled");
-        }
-        catch (Exception e)
-        {
-            SuperController.LogError($"{nameof(SpawnPoint)}.{nameof(OnDisable)}: {e}");
-        }
-    }
+        var root = containingAtom.mainController;
+        var navigationRigTransform = SuperController.singleton.navigationRig.transform;
+        var targetPosition = root.control.position;
+        var rigPosition = navigationRigTransform.position;
+        var targetRotation = root.control.eulerAngles;
+        var rigRotation = navigationRigTransform.eulerAngles;
 
-    public void OnDestroy()
-    {
-        try
-        {
-            SuperController.LogMessage($"{nameof(SpawnPoint)} destroyed");
-        }
-        catch (Exception e)
-        {
-            SuperController.LogError($"{nameof(SpawnPoint)}.{nameof(OnDestroy)}: {e}");
-        }
+        navigationRigTransform.position = new Vector3(targetPosition.x, rigPosition.y, targetPosition.z);
+        navigationRigTransform.eulerAngles = new Vector3(rigRotation.x, targetRotation.y, rigRotation.z);
     }
 }
